6cae7de674c9fe2df308f52c622fea029ffe99c9
losacii <losacii5@gmail.com>
2017-07-12 13:29:02 +0800

    'update'

--- pyOpenCV/git_update.py
+++ pyOpenCV/git_update.py
@@ -4,6 +4,7 @@ import os, time
 orders = "git add . | git commit -m 'update' | git push".split(' | ')
 
 for order in orders:
+    print "\n\n>>>", order
     os.system(order)
     time.sleep(3.5)
 
